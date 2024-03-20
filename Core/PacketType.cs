0693076103cce61c3f81d4e055467cfb6a5d31fe
pyowoojin <20185291@kookmin.ac.kr>
2024-03-21 04:55:47 +0900

    Chatting

---
+++ Core/PacketType.cs
@@ -0,0 +1,18 @@
+﻿namespace Core;
+
+public enum PacketType
+{
+    LoginRequest,
+    LoginResponse,
+    CreateRoomRequest,
+    CreateRoomResponse,
+    RoomListRequest,
+    RoomListResponse,
+    EnterRoomRequest,
+    EnterRoomResponse,
+    UserEnter,
+    UserLeave,
+    Chat,
+    Duplicate,
+    HeartBeat
+}
---
+++ Server/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Server;
+
+internal class Program
+{
+    const string ip = "192.168.219.132";
+
+    static async Task Main(string[] args)
+    {
+        Server server = new Server(ip, 20000, 10);
+        await server.StartAsync();
+    }
+}
